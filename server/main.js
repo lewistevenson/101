f51c40eb280d9d6ba7d44b49517e7d9e7e133c3b
Shaugh McIntosh <shaugh.mcintosh@bendigoadelaide.com.au>
2018-05-16 12:58:17 +1000

    starting to build out the API calls

--- server/main.js
+++ server/main.js
@@ -7,9 +7,35 @@ Meteor.startup(() => {
 const clientId = "sdtLHvS6bTJgSNIPz09J2LUle6JGoCKU";
 const clientSecret = "0MxvJdAKC8TUhxQG";
 
+const telstraAPIEndpoint = "https://tapi.telstra.com/v2";
+
+const authHeaders = {
+    grant_type: "client_credentials",
+    client_id: clientId,
+    client_secret: clientSecret
+};
+
 
 Meteor.methods({
+    "authenticate": () => {
+        HTTP.call("POST", telstraAPIEndpoint + "/oauth/token", { headers: authHeaders }, (err, res) => {
+            if(!err) {
+                Session.set('token', res.data);
+            } else {
+                Session.set('err', true);
+            }
+        });
+
+    },
     "sendSMS": (phoneNumber, message, times) => {
-        HTTP.call()
+        const token = Session.get('token');
+
+        if(token) {
+            const details = {
+                to: phoneNumber,
+                body: message
+            };
+            HTTP.call("POST", telstraAPIEndpoint + "/messages/sms", {data: details, headers: {AccessToken: token}})
+        }
     }
 });
